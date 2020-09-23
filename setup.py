adfa64d90d95b0464d5d08cbb05578b67b3fa698
nic <nwanavit@hatari.cc>
2020-09-24 06:54:37 +0700

    update version number to 9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dynamocacher",
-    version="0.0.8",
+    version="0.0.9",
     author="Nic Wanavit",
     author_email="nwanavit@gmail.com",
     description="storing cache using dynamodb",
