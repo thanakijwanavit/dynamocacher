f26b28c47ce5b53a2acf563fc1d3025888dec67d
nic <nwanavit@hatari.cc>
2020-09-24 08:11:49 +0700

    add setuptools find packages

--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dynamocacher",
-    version="0.0.10",
+    version="0.0.13",
     author="Nic Wanavit",
     author_email="nwanavit@gmail.com",
     description="storing cache using dynamodb dax",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thanakijwanavit/dynamocacher",
-    packages=['dynamocacher'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires = ['pynamodb-dax']
+    install_requires = [
+        'pynamodb-dax',
+        'amazon-dax-client'
+                        ]
 )
